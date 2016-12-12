f52d2ecf1e8faa740f89d13120d0ea4f97a25eaa
Dmitry <dima@paloskin.me>
2016-12-12 17:55:16 +0300

    [fix] add behaviour

--- test/add.test.js
+++ test/add.test.js
@@ -45,8 +45,57 @@ test('Should create and add new inpack module in the master prject when "create"
     create: true
   });
 
-  const modulePath = join(sandbox.path, 'module-does-not-exist');
-  const nodeModulePath = join(sandbox.path, 'node_modules', 'module-does-not-exist');
+  const modulePath = join(sandbox.path, moduleName);
+  const nodeModulePath = join(sandbox.path, 'node_modules', moduleName);
+  const moduleDirectoryStat = await fs.statAsync(modulePath);
+  const mainFileStat = await fs.statAsync(join(modulePath, 'index.js'));
+  const nodeModuleStat = await fs.statAsync(nodeModulePath);
+  const linkPath = await fs.readlinkAsync(join(nodeModulePath, moduleSrcDirName));
+  const pkgBody = await fs.readJsonAsync(join(nodeModulePath, 'package.json'));
+  const inpackJson = await fs.readJsonAsync(join(sandbox.path, inpackConfigName));
+
+  t.true(moduleDirectoryStat.isDirectory());
+  t.true(mainFileStat.isFile());
+  t.true(nodeModuleStat.isDirectory());
+  t.is(linkPath, resolve(modulePath));
+
+  t.deepEqual(pkgBody, {
+    name: moduleName,
+    main: join(moduleSrcDirName, 'index.js'),
+    inpack: true
+  });
+
+  t.deepEqual(inpackJson.modules, {
+    [moduleName]: {
+      path: moduleName,
+      package: {
+        name: moduleName,
+        main: join(moduleSrcDirName, 'index.js'),
+        inpack: true
+      }
+    }
+  });
+
+  await sandbox.remove();
+});
+
+test('Should create and add new inpack module outside master project', async t => {
+
+  const sandbox = await createSandbox({
+    structure: 'deep',
+    isMaster: true,
+    noPrefix: true
+  });
+
+  const moduleName = 'level1';
+  const inpack = new Inpack();
+
+  await inpack.add(join(sandbox.path, moduleName), {
+    create: true
+  });
+
+  const modulePath = join(sandbox.path, moduleName);
+  const nodeModulePath = join(sandbox.path, 'node_modules', moduleName);
   const moduleDirectoryStat = await fs.statAsync(modulePath);
   const mainFileStat = await fs.statAsync(join(modulePath, 'index.js'));
   const nodeModuleStat = await fs.statAsync(nodeModulePath);
