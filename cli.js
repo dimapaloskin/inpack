21b67248f14dd4fa6afce204fff6e0cddc3de28c
Dmitry <dima@paloskin.me>
2016-12-13 17:25:14 +0300

    add/remove

--- cli.js
+++ cli.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 
 require('babel-core/register');  // eslint-disable-line import/no-unassigned-import
-require('./lib/cli'); // eslint-disable-line import/no-unassigned-import
+const cli = require('./lib/cli');
+
+cli();
